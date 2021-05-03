7d73c5408b3c208a5d09ce4c158a0a198bbef5a3
mathilderenversade <mathirenversade@gmail.com>
2021-05-03 14:21:42 +0200

    variables et console log en javascript

---
+++ cours.js
@@ -0,0 +1,14 @@
+// Il existe en javascript des variables. Pour les déclarer on a plusieurs options. 
+// option 1, utiliser l'attribut var
+var bonjour = "bonjour";
+console.log(bonjour);
+console.log(typeof(bonjour));
+
+// option 2, utiliser l'attribut let 
+let bonsoir = "bonsoir";
+console.log(bonsoir);
+
+// option 3, utiliser l'attribut const
+const aurevoir = "aurevoir";
+console.log(aurevoir);
+
